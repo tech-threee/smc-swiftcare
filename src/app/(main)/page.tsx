9aba23ce7b981a7c690ccc9a8188d6d3d3ca5af5
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-03-15 12:18:50 +0000

    infographics

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,32 @@
+import DashboardCard from "@/components/cards/dashboard-card";
+import { dashBoardIconsMap } from "@/utils/client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between px-2">
+      <section className="grid gap-4  w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <DashboardCard
+          label="Total Staff"
+          value={400}
+          icon={dashBoardIconsMap.staff}
+          link="/staff"
+        />
+        <DashboardCard
+          label="Total Messages"
+          value={1000}
+          icon={dashBoardIconsMap.messages}
+          link="/messages"
+
+        />
+        <DashboardCard
+          label="Total Posts"
+          value={20}
+          icon={dashBoardIconsMap.posts}
+          link="/blog"
+
+        />
+      </section>
     </main>
   );
 }
