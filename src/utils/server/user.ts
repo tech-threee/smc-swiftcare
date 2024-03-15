ff0a803dbaec183c78e361957d7b43c389e412bc
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-03-15 15:34:45 +0000

    user server functions

--- src/utils/server/user.ts
+++ src/utils/server/user.ts
@@ -1,6 +1,81 @@
-import { ApiResponse, ChangePasswordInput, LoginUserInput, ResetPasswordInput, SendCodeInput, UpdateUserDetailsInput, User, UserRes, VerifyCodeInput } from "@/types"
+import { ApiResponse, ChangePasswordInput, LoginUserInput, ResetPasswordInput, SendCodeInput, UpdateUserDetailsInput, User, UserRes, UserRoles, VerifyCodeInput } from "@/types"
 import Axios from "../axios";
 import _ from "lodash"
+
+type CreateUserInput = Pick<User, "email" | "othernames" | "surname" | "phone">
+
+export const rolesMap = {
+    "IT": "it",
+    "DOCTOR": "doctor",
+    "NURSE": "nurse",
+    "PHARMACIST": "pharmacist",
+    "SUDO": "sudo",
+}
+
+
+export const CREATE_USER = async (info: CreateUserInput,  token: string) => {
+    try {
+        const response: ApiResponse<UserRes> = await Axios({
+            method: "POST",
+            url: `/staff/`,
+            data: info,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 200 || response.status === 201) {
+            return response.data.data;
+        } else {
+            throw new Error("oops");
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const GET_USER_BY_ID = async (id: string, role: UserRoles, token: string) => {
+
+    try {
+        const response: ApiResponse<UserRes> = await Axios({
+            method: "GET",
+            url: `/${rolesMap[role]}/${id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data.data;
+        } else {
+            throw new Error("oops");
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const GET_USERS = async ( token: string) => {
+
+    try {
+        const response: ApiResponse<UserRes[]> = await Axios({
+            method: "GET",
+            url: `/staff/`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 200) {
+            return response.data.data;
+        } else {
+            throw new Error("oops");
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
 
 export const UPDATE_USER = async (info: UpdateUserDetailsInput, token: string) => {
     try {
@@ -36,6 +111,28 @@ export const CHANGE_USER_PASSWORD = async (info: ChangePasswordInput, token: str
         });
 
         if (response.status === 200 || response.status === 201) {
+            return response.data.data;
+        } else {
+            throw new Error("oops");
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const DELETE_USER = async (id: string, token: string) => {
+
+    try {
+        const response: ApiResponse<UserRes> = await Axios({
+            method: "DELETE",
+            url: `/staff/${id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        if (response.status === 200) {
             return response.data.data;
         } else {
             throw new Error("oops");
